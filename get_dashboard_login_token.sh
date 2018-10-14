a66da5a10d86c81956f2d910ac57391244b8aeb6
David Balla <szefoka@gmail.com>
2018-10-15 00:34:47 +0200

    get_dashboard_login_token.sh prints the address of the dashboard

--- get_dashboard_login_token.sh
+++ get_dashboard_login_token.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+DASH_ADDR=$(ifconfig eno49 | grep "inet addr:" | awk '{print $2}' | cut -c6-):$(kubectl get services -n kube-system | grep kubernetes-dashboard | awk '{ print $5 }' | cut -d ':' -f 2 | cut -d '/' -f 1)
+
+echo "Kubernetes dashboard is running on https://$DASH_ADDR"
 echo "Token:"
 kubectl get secret -n kube-system | grep k8sadmin | cut -d " " -f1 | xargs -n 1 | xargs kubectl get secret  -o 'jsonpath={.data.token}' -n kube-system | base64 --decode
 echo
