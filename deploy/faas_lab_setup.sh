e484f78e6fc222cce4d2f5430993a7d370708d11
David Balla <szefoka@gmail.com>
2018-10-16 23:47:40 +0200

    weavescope added

--- deploy/faas_lab_setup.sh
+++ deploy/faas_lab_setup.sh
@@ -27,5 +27,6 @@ curl -sSL https://cli.openfaas.com | sudo sh
 #kubectl port-forward -n monitoring alertmanager-kube-prometheus-0 9093 &
 
 ./kubernetes_dashboard_install.sh
+./weave_scope_setup.sh
 
 ssh node2 -o "StrictHostKeyChecking no" apt-get update && apt-get install apache2-utils
