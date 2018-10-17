347ac5f0468ba6887a89fc23cb140af3a485de90
David <davidballa1992@gmail.com>
2018-10-17 08:28:11 +0200

    weave-scope portforward fixed

--- deploy/weave_scope_setup.sh
+++ deploy/weave_scope_setup.sh
@@ -1,3 +1,3 @@
 kubectl apply -f "https://cloud.weave.works/k8s/scope.yaml?k8s-version=$(kubectl version | base64 | tr -d '\n')"
-kubectl port-forward -n weave "$(kubectl get -n weave pod --selector=weave-scope-component=app -o jsonpath='{.items..metadata.name}')" 4040
+kubectl port-forward -n weave "$(kubectl get -n weave pod --selector=weave-scope-component=app -o jsonpath='{.items..metadata.name}')" 4040  &
 sudo iptables -t nat -I PREROUTING -p tcp --dport 4040 -j DNAT --to 127.0.0.1:4040
