13c54f98cccf468d706e44abd3a8b09e3001a8aa
David Balla <szefoka@gmail.com>
2018-10-31 22:57:25 +0100

    sleep removed from weavescope setup

--- deploy/weave_scope_setup.sh
+++ deploy/weave_scope_setup.sh
@@ -1,5 +1,16 @@
+function wait_for_weavescope {
+  while [[ "$(kubectl get pods -n weave | grep weave-scope-agent | grep Running | wc -l)" -lt  "$(kubectl get nodes | tail -n +2 | wc -l)" ]];
+  do
+    sleep 1
+  done
+  while [[ "$(kubectl get pods -n weave | grep weave-scope-app | grep Running | wc -l)" -lt 1 ]];
+  do
+    sleep 1
+  done
+}
+
 kubectl apply -f "https://cloud.weave.works/k8s/scope.yaml?k8s-version=$(kubectl version | base64 | tr -d '\n')"
-sleep 10
+wait_for_weavescope
 kubectl port-forward -n weave "$(kubectl get -n weave pod --selector=weave-scope-component=app -o jsonpath='{.items..metadata.name}')" 4040  &
 #insecure, use from localhost
 #sudo iptables -t nat -I PREROUTING -p tcp --dport 4040 -j DNAT --to 127.0.0.1:4040
