a07f8bdbb29103f5a9113ee003d9b58fc7f9d61c
David Balla <szefoka@gmail.com>
2018-11-27 09:27:21 +0100

    elk_setup.sh fixed

--- deploy/elk_setup.sh
+++ deploy/elk_setup.sh
@@ -1,9 +1,9 @@
 sysctl -w vm.max_map_count=262144
 ssh node2 -o "StrictHostKeyChecking no" sysctl -w vm.max_map_count=262144
-elk/kubectl create -f elasticsearch_dep.yaml
-elk/kubectl create -f elasticsearch_svc.yaml
-elk/kubectl create -f kibana_dep.yaml
-elk/kubectl create -f kibana_svc.yaml
+kubectl create -f elk/elasticsearch_dep.yaml
+kubectl create -f elk/elasticsearch_svc.yaml
+kubectl create -f elk/kibana_dep.yaml
+kubectl create -f elk/kibana_svc.yaml
 ELASTIC_SEARCH_IP=$(kubectl get services | grep elasticsearch | awk '{print $3}')
 curl https://raw.githubusercontent.com/elastic/beats/master/deploy/kubernetes/filebeat-kubernetes.yaml > elk/filebeat-kubernetes.yaml
 sed -i "s/value: elasticsearch/value: $ELASTIC_SEARCH_IP/" elk/filebeat-kubernetes.yaml
