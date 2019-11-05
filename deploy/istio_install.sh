81d5dade0c5ebdc8188e965a29636fd7b3c27013
root <root@node1.kubernetes.cloudnet-pg0.utah.cloudlab.us>
2019-11-05 02:28:15 -0700

    istio 1.3.4 added

--- deploy/istio_install.sh
+++ deploy/istio_install.sh
@@ -1,11 +1,13 @@
+helm repo add istio.io https://storage.googleapis.com/istio-release/releases/1.3.4/charts/
 git clone https://github.com/istio/istio.git
 cd istio
-git checkout release-1.0
-helm repo add istio.io https://storage.googleapis.com/istio-release/releases/1.1.0-snapshot.3/charts
-helm dep update install/kubernetes/helm/istio/
-helm upgrade --install istio --namespace istio-system install/kubernetes/helm/istio/
+kubectl create namespace istio-system
+helm template install/kubernetes/helm/istio-init --name istio-init --namespace istio-system | kubectl apply -f -
+sleep 30
+helm template install/kubernetes/helm/istio --name istio --namespace istio-system | kubectl apply -f -
 cd ..
-#wget https://github.com/istio/istio/releases/download/1.1.0-snapshot.3/istio-1.1.0-snapshot.3-linux.tar.gz
-wget https://github.com/istio/istio/releases/download/1.0.4/istio-1.0.4-linux.tar.gz
-tar -xf istio-1.1.0-snapshot.3-linux.tar.gz
-cp istio-1.1.0-snapshot.3/bin/istioctl /usr/bin/
+wget https://github.com/istio/istio/releases/download/1.3.4/istio-1.3.4-linux.tar.gz
+tar -xvf istio-1.3.4-linux.tar.gz
+cp istio-1.3.4/bin/istioctl /usr/bin
+
+
